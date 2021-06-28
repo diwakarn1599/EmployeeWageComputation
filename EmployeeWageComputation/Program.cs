29387ed7ace8c7ad72cd9edc96ca77a554c563ea
Diwakar <diwakarn1599@gmail.com>
2021-06-28 13:39:00 +0530

    Employee wage Usecase 1 completed

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -4,10 +4,24 @@ namespace EmployeeWageComputation
 {
     class Program
     {
+        public const int PRESENT = 1;
+        public const int ABSENT = 0;
         static void Main(string[] args)
         {
             
             Console.WriteLine("Welcome to Employee Wage Computation Problem");
+            Random rand = new Random(); // intializing random class
+            int empInput = rand.Next(0, 2);
+            if(empInput == PRESENT)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            
+
         }
     }
 }
