48392c3235c2877934ce356558bbc5852da0ab28
Diwakar <diwakarn1599@gmail.com>
2021-06-28 17:06:11 +0530

    employee wage Use case 6 completed

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -9,35 +9,38 @@ namespace EmployeeWageComputation
         public const int ABSENT = 0;
         public const int PART_TIME = 2;
         public const int WAGE_PER_HOUR = 20;
-        public const int WORKING_DAYS = 20;
+        public const int WORKING_DAYS_PER_MONTH = 20;
+        public const int WORKING_HOURS_PER_MONTH = 100;
 
         static void Main(string[] args)
         {
+            int workingDays = 0;
             int wagesForMonth = 0;   
             int workingHours = 0;
             int totalWorkingHours = 0;
             Console.WriteLine("Welcome to Employee Wage Computation Problem");
             Random rand = new Random(); // intializing random class
             //for loop for calculating for 20 days
-            for (int day = 1; day <= WORKING_DAYS; day++)
+            while(totalWorkingHours <= WORKING_HOURS_PER_MONTH && workingDays < WORKING_DAYS_PER_MONTH)
             {
                 int empInput = rand.Next(0, 3);//Generating random b/w 0 and 3
                 switch (empInput)
                 {
                     case FULL_TIME:
-                        //Console.WriteLine("Employee is Present Full Time");
+                        Console.WriteLine("Employee is Present Full Time");
                         workingHours = 8;
                         break;
                     case PART_TIME:
-                        //Console.WriteLine("Employee is Present Part Time");
+                        Console.WriteLine("Employee is Present Part Time");
                         workingHours = 4;
                         break;
                     default:
-                        //Console.WriteLine("Employee is Absent");
+                        Console.WriteLine("Employee is Absent");
                         workingHours = 0;
                         break;
                 }
                 totalWorkingHours += workingHours;
+                workingDays++;
 
             }
 
