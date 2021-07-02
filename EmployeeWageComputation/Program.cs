644bba8413145b70882bcf9bb153c0a87b14f494
Diwakar <diwakarn1599@gmail.com>
2021-07-02 16:44:48 +0530

    [Diwakar] Update - implementation through class variables and methods

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -12,30 +12,31 @@ namespace EmployeeWageComputation
         public const int WORKING_DAYS_PER_MONTH = 20;
         public const int WORKING_HOURS_PER_MONTH = 100;
 
-        static void Main(string[] args)
+
+        private static void CalculateEmpWage()
         {
             int workingDays = 0;
-            int wagesForMonth = 0;   
+            int wagesForMonth = 0;
             int workingHours = 0;
             int totalWorkingHours = 0;
             Console.WriteLine("Welcome to Employee Wage Computation Problem");
             Random rand = new Random(); // intializing random class
             //for loop for calculating for 20 days
-            while(totalWorkingHours <= WORKING_HOURS_PER_MONTH && workingDays < WORKING_DAYS_PER_MONTH)
+            while (totalWorkingHours <= WORKING_HOURS_PER_MONTH && workingDays < WORKING_DAYS_PER_MONTH)
             {
                 int empInput = rand.Next(0, 3);//Generating random b/w 0 and 3
                 switch (empInput)
                 {
                     case FULL_TIME:
-                        Console.WriteLine("Employee is Present Full Time");
+                        //Console.WriteLine("Employee is Present Full Time");
                         workingHours = 8;
                         break;
                     case PART_TIME:
-                        Console.WriteLine("Employee is Present Part Time");
+                        //Console.WriteLine("Employee is Present Part Time");
                         workingHours = 4;
                         break;
                     default:
-                        Console.WriteLine("Employee is Absent");
+                        //Console.WriteLine("Employee is Absent");
                         workingHours = 0;
                         break;
                 }
@@ -45,8 +46,13 @@ namespace EmployeeWageComputation
             }
 
             wagesForMonth = WAGE_PER_HOUR * totalWorkingHours;//formula for wages
-            Console.WriteLine("Employee wage per month is "+ wagesForMonth);
-            
+            Console.WriteLine("Employee wage per month is " + wagesForMonth);
+
+        }
+
+        static void Main(string[] args)
+        {
+           CalculateEmpWage();
 
         }
     }
