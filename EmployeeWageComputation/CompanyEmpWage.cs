9c05badab9093d76e311a399b29cabd3b113ddab
Diwakar <diwakarn1599@gmail.com>
2021-07-04 23:53:20 +0530

    [Diwakar] Manage multiple wage of multiple company

---
+++ EmployeeWageComputation/CompanyEmpWage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageComputation
+{
+    class CompanyEmpWage
+    {
+        public string company;
+        public int wagePerHour;
+        public int workingDaysPerMonth;
+        public int maxWorkingHours;
+        public int wagesForMonth;
+
+        public CompanyEmpWage(string comp, int wagePerHour, int workingDaysPerMonth, int maxWorkingHours)
+        {
+            this.company = comp;
+            this.wagePerHour = wagePerHour;
+            this.workingDaysPerMonth = workingDaysPerMonth;
+            this.maxWorkingHours = maxWorkingHours;
+        }
+
+        public void SetWagesForMonth(int swfm)
+        {
+            this.wagesForMonth = swfm;
+        }
+        public string toString()
+        {
+            return $"Total EmpWage For company {this.company} is {this.wagesForMonth}";
+        }
+    }
+}
--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -11,20 +11,29 @@ namespace EmployeeWageComputation
         //public const int WAGE_PER_HOUR = 20;
         //public const int WORKING_DAYS_PER_MONTH = 20;
         //public const int WORKING_HOURS_PER_MONTH = 100;
-        private string company;
-        private int wagePerHour;
-        private int workingDaysPerMonth;
-        private int maxWorkingHours;
-        private int wagesForMonth;
+        
+        private int  noOfCompanies = 0;
+        private CompanyEmpWage[] CompanyEmpArray;
 
-        public EmployeeWage(string comp, int wagePerHour, int workingDaysPerMonth, int maxWorkingHours)
+        public EmployeeWage()
         {
-            this.company = comp;
-            this.wagePerHour = wagePerHour;
-            this.workingDaysPerMonth = workingDaysPerMonth;
-            this.maxWorkingHours = maxWorkingHours;
+            this.CompanyEmpArray = new CompanyEmpWage[5];
         }
-        private void CalculateEmpWage()
+
+        public void AddWage(string comp, int wagePerHour, int workingDaysPerMonth, int maxWorkingHours)
+        {
+            CompanyEmpArray[this.noOfCompanies++] = new CompanyEmpWage( comp,  wagePerHour,  workingDaysPerMonth,  maxWorkingHours);
+        }
+
+        public void ComputeEmpWage()
+        {
+            for(int i = 0; i < noOfCompanies; i++)
+            {
+                CompanyEmpArray[i].SetWagesForMonth(this.CalculateEmpWage(this.CompanyEmpArray[i]));
+                Console.WriteLine(this.CompanyEmpArray[i].toString());
+            }
+        }
+        private int CalculateEmpWage(CompanyEmpWage company)
         {
             int workingDays = 0;
             
@@ -33,7 +42,7 @@ namespace EmployeeWageComputation
             
             Random rand = new Random(); // intializing random class
             //for loop for calculating for 20 days
-            while (totalWorkingHours <= this.maxWorkingHours && workingDays < this.workingDaysPerMonth)
+            while (totalWorkingHours <= company.maxWorkingHours && workingDays < company.workingDaysPerMonth)
             {
                 int empInput = rand.Next(0, 3);//Generating random b/w 0 and 3
                 switch (empInput)
@@ -56,26 +65,28 @@ namespace EmployeeWageComputation
 
             }
 
-            this.wagesForMonth = this.wagePerHour * totalWorkingHours;//formula for wages
+            return company.wagePerHour * totalWorkingHours;//formula for wages
             //Console.WriteLine($"Total EmpWage For company {company} is {wagesForMonth}");
 
         }
-        public string toString()
-        {
-            return $"Total EmpWage For company {this.company} is {this.wagesForMonth}";
-        }
+        
 
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Problem");
+            EmployeeWage emp = new EmployeeWage();
+            emp.AddWage("Tvs", 50, 20, 100);
+            emp.AddWage("Zoho", 40, 20, 200);
+            emp.ComputeEmpWage();
+
             //parameterized constructor for employee wage class to initialize tvs variables
-            EmployeeWage tvs = new EmployeeWage("Tvs", 50, 20, 100);
-            tvs.CalculateEmpWage();
-            Console.WriteLine(tvs.toString());
-            //parameterized constructor for employee wage class to initialize zoho variables
-            EmployeeWage zoho = new EmployeeWage("Zoho", 40, 20, 200);
-            zoho.CalculateEmpWage();
-            Console.WriteLine(zoho.toString());
+            //EmployeeWage tvs = new EmployeeWage("Tvs", 50, 20, 100);
+            //tvs.CalculateEmpWage();
+            //Console.WriteLine(tvs.toString());
+            ////parameterized constructor for employee wage class to initialize zoho variables
+            //EmployeeWage zoho = new EmployeeWage("Zoho", 40, 20, 200);
+            //zoho.CalculateEmpWage();
+            //Console.WriteLine(zoho.toString());
 
             //CalculateEmpWage("Tvs",50,20,100);
             //CalculateEmpWage("Zoho", 40, 20, 200);
